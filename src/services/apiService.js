0bb0355a621b4190dbdef981c2e0a5524fa57f89
reachnou <rnou@miu.edu>
2023-10-24 15:11:07 -0500

    config header on endpoint

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -4,11 +4,11 @@ import http from "../config/http";
 export default function apiService(endpoint) {
 
     this.getAll = () => {
-        return http.get(endpoint);
+        return http.get(endpoint, {headers: header()});
     }
 
     this.getById = (id) => {
-        return http.get(`${endpoint}/${id}`);
+        return http.get(`${endpoint}/${id}`,{headers: header()});
     }
 
     this.add = (data) => {
@@ -16,10 +16,10 @@ export default function apiService(endpoint) {
     }
 
     this.update = (id, data) => {
-        return http.put(`${endpoint}/${id}`, data);
+        return http.put(`${endpoint}/${id}`, data, {headers: header()});
     }
 
     this.remove = (id) => {
-        return http.delete(`${endpoint}/${id}`);
+        return http.delete(`${endpoint}/${id}`, {headers: header()});
     }
 }
