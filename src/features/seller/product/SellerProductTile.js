405fb30cf2888ace8d21bb7ab8c72e04c374013a
reachnou <rnou@miu.edu>
2023-10-24 15:04:51 -0500

    fix bugs

--- src/features/seller/product/SellerProductTile.js
+++ src/features/seller/product/SellerProductTile.js
@@ -25,9 +25,9 @@ export default function SellerProductTile(props) {
                  src="https://jennifermaker.com/wp-content/uploads/gift-box-templates-cricut-f-735x735.jpg"
                  width="80px" height="80px" alt=""/>
         </td>
-        <td className="align-middle">{product.title}</td>
-        <td className="align-middle">${product.auction.startPrice}</td>
-        <td className="align-middle">{checkReleased(product.released)}</td>
+        <td className="align-middle">{product?.title}</td>
+        <td className="align-middle">${product?.auction?.startPrice}</td>
+        <td className="align-middle">{checkReleased(product?.released)}</td>
         <td className="align-middle">
             <button className="btn btn-secondary mx-1" onClick={goProductDetail}><i className="bi bi-eye"></i></button>
             <button className="btn btn-secondary mx-1" onClick={goEditProduct}><i className="bi bi-pencil"></i></button>
